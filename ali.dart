2aaacd122ff4ce0161ca6a6324e22e5f90b80282
shahbazali <shahbazali4203@gmail.com>
2024-01-05 10:08:47 +0500

    dart code

---
+++ ali.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+void main(){
+// for (int a=0;a<=10;a++){
+
+// stdout.write('$a');
+// }
+// for(int b=0;b<=b*2;b++){
+
+// }
+// print('*');
+
+int a=0;
+while(a<10){
+  print(a);
+  a++;
+}
+int b=0;
+while(b<10){
+  print(b);
+  b++;
+}
+int A=0;
+while(a<=10){
+if(a%2==1){
+  print(a);
+}
+}
+
+  a++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ shahbaz.dart
@@ -0,0 +1,194 @@
+
+
+
+import 'dart:io';
+
+void main(){
+print('shahbaz');
+
+///int A = (45);
+//int B = (37);
+//print(A<B);
+//if (A<B || true);
+//String  num = "ali";
+//String num2 = 'reat';
+//print(num  + num2);
+
+//bool String1 = (true);
+//print(String1);
+
+//int totalamount = 660;
+//int hundradrem = totalamount % 100;
+//double hundradconts = totalamount/100;
+//print('hundrad notes:$hundradconts');
+//int  fiftyrem =hundradrem % 50;
+//double fiftycont =hundradrem/50;
+//print('fiftycont:${fiftycont}');
+//int tewnrem = hundradrem% 20;
+//double tewncont= hundradrem /20;
+//print('tewncont:${tewncont}');
+//int tenrem = hundradrem % 10;
+//double tencont = hundradrem /10;
+//print('tencont:${tencont}');
+
+//stdout.write("Enter first value is a ");
+//int.parse(stdin.readLineSync()!);
+//stdout.write("Enter second value is b ");
+//int.parse(stdin.readLineSync()!);
+//stdout.write("Enter third value is c ");
+//int.parse(stdin.readLineSync()!);
+//int a = 125;
+//int b = 98;
+//int c =100;
+//
+///if (a>b && a>c);
+//print("a is greater  $a" );
+//if (b>c){
+//print("b is middel $b ");
+//print("c is small $c ");
+//}else{  
+//  print("c is middel $c ");
+//print("b is small $b ");
+//}
+//if (b>a && b>c);
+//print("b is greater  $b" );
+//if (b>c){
+//print("a is middel $a ");
+//print("c is small $c ");
+//}else{  
+//  print("c is middel $c ");
+//print("a is small $a ");
+//}
+//if (c>a && c>b);
+//print("c is greater  $c" );
+//if (b>c){
+//print("b is middel $b ");
+//print("a is small $a ");
+//}else{  
+//  print("a is middel $a ");
+//print("b is small $b ");
+//}
+//
+
+// stdout.write('enter any Number');
+// var a = int.parse(stdin.readLineSync()!);
+// stdout.write("inter wrong number");
+// var b = int.parse(stdin.readLineSync()!);
+
+
+// if (a>b){
+//   print ("a is greater $a");
+// print('B is greater $b');
+// }else{print('B is greater $b');
+// print('a is greater $a');
+// // }
+// var a1 = 46;
+// print(a1 is double);
+// int sum1 = 56;
+// int sum2 = 45;
+// print(sum1 + sum2);
+
+// var eng , math ,urdu, dart, sum;
+// double percentage;
+
+// stdout.write('enter english marks');
+// eng = int.parse(stdin.readLineSync()!);
+
+// stdout.write('enter math marks');
+// math = int.parse(stdin.readLineSync()!);
+
+// stdout.write('enter urdu marks');
+// urdu = int.parse(stdin.readLineSync()!);
+
+// stdout.write('enter dart marks');
+// dart = int.parse(stdin.readLineSync()!);
+
+// if(eng <=100 && math <=100 && urdu <=100 && dart<=100);
+
+// print('english marks $eng');
+// print('math marks $math');
+// print('urdu marks $urdu');
+// print('dart marks $dart');
+
+// sum = eng + math + urdu + dart ;
+// print('total marks is 600 obtain mark $sum');
+
+// percentage = sum/600*100;
+// print('percentage ${percentage.round()}');
+
+// if (percentage>= 86){
+//   print('grade is A');
+// }else if(percentage>=76 && percentage <86){
+//   print('grade is B');
+// }else if(percentage>=69 && percentage<76){
+//   print('grade is C');
+// }else if(percentage>=50 && percentage<69){
+//   print('fail');
+// }else{
+// print('please entar lass then 100 marks');
+// }
+
+
+// int a= 5;
+// switch(a){
+//   case 1:
+// print('monday');
+// break;
+// case 2:
+// print('tuesday');
+// break;
+// case 3:
+// print('wednesday');
+// break;
+// case 4:
+// print('thusday');
+// break;
+// case 5:
+// print('fariday');
+// break;
+// case 6:
+// print('sataday');
+// break;
+// case 7:
+// print('sunday');
+// break;
+// }
+// var num1 =34;
+// var num2 =23;
+// String operator="*";
+// switch(operator){
+//   case "+":
+//   print('plus${num1 + num2}');
+//   break;
+//   case "*":
+//   print('multiple${num1 * num2}');
+//   break;
+//   case "-":
+//   print('minus${num1 - num2}');
+//   break;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
